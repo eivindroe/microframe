338e51d10fd777c1db380ab2ee98c7ddecba1ff9
Eivind Røe <eivindroe@gmail.com>
2016-08-22 19:18:24 +0200

    Removed notice about Slim, as this framework does not use Slim anymore

--- load.php
+++ load.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * Step 1: Require the Slim Framework
- *
- * If you are not using Composer, you need to require the
- * Slim Framework and register its PSR-0 autoloader.
- *
- * If you are using Composer, you can skip this step.
- */
 require 'System/Crypt.php';
 require 'Config/Config.php';
 require 'System/Request.php';
